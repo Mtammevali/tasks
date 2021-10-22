fbb518212075371e2a3e2d6aa2cb51d852e4c733
Marek Tammevali <marek.tammevali@khk.ee>
2021-10-22 15:24:33 +0300

    Added task functionality

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("test");
+
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", addTask);
+
+
+function addTask(event){
+    //get form input data.
+    const taskInput = document.querySelector("#task");
+    let task = taskInput.value;
+
+    //create list item element.
+    const li = document.createElement("li")
+    li.className = "collection-item";
+    //create text element.
+    const text = document.createTextNode(task);
+   li.appendChild(text);
+   //add list item to UL
+    const ul = document.querySelector(".collection");
+    ul.appendChild(li);
+
+
+    event.preventDefault();
+
+}
+
