e62d7bbf1608c5555af1e24542a52e43b1153db2
Marek Tammevali <marek.tammevali@khk.ee>
2021-10-22 16:15:27 +0300

    Added delete functionality

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
 
+// event elements
+const form = document.querySelector('form');
+const taskList = document.querySelector('.collection');
 
-const form = document.querySelector("form");
+// events
+form.addEventListener('submit', addTask);
+taskList.addEventListener('click', deleteTask);
 
-form.addEventListener("submit", addTask);
-
+function deleteTask(event){
+    if(event.target.textContent === 'X'){
+        if(confirm('Do you want to delete this task?')){
+            event.target.parentElement.remove();
+        }
+    }
+}
 
 function addTask(event){
-    //get form input data.
-    const taskInput = document.querySelector("#task");
+    // get form input data
+    const taskInput = document.querySelector('#task');
     let task = taskInput.value;
-
-    //create list item element.
-    const li = document.createElement("li")
-    li.className = "collection-item";
-    //create text element.
+    // create <li> element
+    const li = document.createElement('li');
+    // add css class
+    li.className = 'collection-item';
+    // create text element
     const text = document.createTextNode(task);
-   li.appendChild(text);
-   //add list item to UL
-    const ul = document.querySelector(".collection");
+    // add text to <li>
+    li.appendChild(text);
+    // create <a> element
+    const link = document.createElement('a');
+    // add css class
+    link.className = 'secondary-content';
+    // set href atribute to <a>
+    link.setAttribute('href', '#');
+    // add text content to <a>
+    link.appendChild(document.createTextNode('X'));
+    // add <a> to <li>
+    li.appendChild(link);
+    // add li to ul
+    const ul = document.querySelector('.collection');
     ul.appendChild(li);
-
-
+    taskInput.value ="";
     event.preventDefault();
-
 }
+
+
+
+
+
+
+
 
