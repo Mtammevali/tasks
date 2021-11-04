cb44ccaa1f4e2ac341d18be39d7bf6df59c2426a
Marek Tammevali <marek.tammevali@khk.ee>
2021-11-04 22:33:42 +0200

    Added  delete functions single and all task value to local storage!

--- app.js
+++ app.js
@@ -18,13 +18,37 @@ function deleteTasks(event){
         }
 
 }
+function deleteAllTaskFromLocalStorage(){
+    // localStorage.clear();
+    if(localStorage.getItem('tasks') === null){
+        let tasks = [];
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+    localStorage.removeItem('tasks');
+}
 
 function deleteTask(event){
     if(event.target.textContent === 'X'){
         if(confirm('Do you want to delete this task?')){
             event.target.parentElement.remove();
+            task = event.target.parentElement.firstChild.textContent;
+            deleteTaskFromLocalStorage(task);
         }
     }
+}
+function deleteTaskFromLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function (tasksElement, index){
+        if(tasksElement === task){
+            tasks.splice(index, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function addTask(event){
